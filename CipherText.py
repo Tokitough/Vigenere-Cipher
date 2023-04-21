6ae3f334f4c0348f6c5c40d7d61581593e26ecbf
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-04-21 14:40:35 +0800

    Implement key padding

--- CipherText.py
+++ CipherText.py
@@ -1,1 +1,24 @@
+# implement key padding method
+def _pad_key(plaintext, key):
+    paddedKey = ''
+    i = 0
+    
+# loop over the characters
+    for char in plaintext:
+        if char.isalpha():
+            paddedKey += key[i % len(key)]
+            i += 1
+        else:
+            paddedKey += ' '
+    return paddedKey
+
+# implement ecrypt_decrypt method
+    # define the first alphabet letter to get position in alphabet
+    # get the position of the plaintext character
+    # formula for encryption or decryption
+    # get character of the ASCII code
+# return ciphertext
+# implement decrypt method to get plaintext
+# ask user to input message
+# print output
 
